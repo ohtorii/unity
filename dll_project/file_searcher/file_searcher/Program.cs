7e17bd6900f4a37494df3e7b6f50ec919f861ad6
ohtorii <ikeuchi.h9@gmail.com>
2019-08-14 08:59:38 +0900

    Create new project.

---
+++ dll_project/file_searcher/file_searcher/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace file_searcher
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
